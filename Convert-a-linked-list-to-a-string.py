93dfcf133171f1446584e52cfd0e55d87850887b
Yarynka <jaryna.zhvanko@gmail.com>
2026-03-22 17:42:18 +0200

    hope that's all

--- Convert-a-linked-list-to-a-string.py
+++
@@ -1,9 +0,0 @@
-def stringify(node):
-    """jvh"""
-    if not node:
-        return 'None'
-    st=str(node.data)+' -> '
-    while node.next:
-        node=node.next
-        st+=str(node.data)+' -> '
-    return st+'None'
